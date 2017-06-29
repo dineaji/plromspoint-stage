d07539dda749df0e9764b4f604cfe9f505823778
dinesh <dinesh.kumar2@mattel.com>
2017-06-29 11:53:32 +0000

    v3

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var bodyParser = require('body-parser');
 
 
 var index = require('./routes/index');
-//var users = require('./routes/users');
-//var dbconnect = require('./routes/dbconnect');
+var users = require('./routes/users');
+var dbconnect = require('./routes/dbconnect');
 
 var app = express();
 
@@ -27,8 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/users', users);
-//app.use('/server', dbconnect);
+app.use('/users', users);
+app.use('/server', dbconnect);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
