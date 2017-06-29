c3af121cf9f1c8e175da3d8c50161bae59d0bb4a
Dinesh Kumar <dineaji@gmail.com>
2017-06-29 16:03:20 +0530

    application error - v2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongodb = require('Mongodb')
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
-var dbconnect = require('./routes/dbconnect');
+//var dbconnect = require('./routes/dbconnect');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 //app.use('/users', users);
-app.use('/server', dbconnect);
+//app.use('/server', dbconnect);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
