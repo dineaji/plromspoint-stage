ebe3f151c48aa350f8c54001fdec3d3f037b892b
Dinesh Kumar <dineaji@gmail.com>
2017-06-29 15:59:17 +0530

    application error - v1

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 
 var mongodb = require('Mongodb')
 
-var index = require('./routes/dbconnect');
-var users = require('./routes/users');
+var index = require('./routes/index');
+//var users = require('./routes/users');
 var dbconnect = require('./routes/dbconnect');
 
 var app = express();
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 app.use('/server', dbconnect);
 
 // catch 404 and forward to error handler
